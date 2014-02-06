15b83b325147bd29b610a512fcd2356a9e5bd303
Rex Chung <rex@rorcraft.com>
2014-02-05 16:45:08 -0800

    parse port argument

--- serve/serve.go
+++ serve/serve.go
@@ -3,9 +3,16 @@ import (
   "net/http"
   "os"
   "fmt"
+  "flag"
 )
+
+var port = flag.String("p", "8080", "specify the port number")
+
 func main() {
+  flag.Parse()
   cwd, _ := os.Getwd()
-  fmt.Println("Serving files from ", cwd)
-  panic(http.ListenAndServe(":8080", http.FileServer(http.Dir(cwd))))
+  fmt.Println("Serving files from", cwd)
+  fmt.Println("Listening on port", *port)
+  serving := ":" + *port
+  panic(http.ListenAndServe(serving, http.FileServer(http.Dir(cwd))))
 }
